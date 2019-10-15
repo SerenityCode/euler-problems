ea26e1178247ce5e4e497ad642d32b3882f22de4
Jeffrey Whitehead <jeff@bitposter.co>
2019-10-15 14:13:36 +0100

    Optimise euler 03 solution

--- solutions/euler03/euler03.py
+++ solutions/euler03/euler03.py
@@ -4,21 +4,35 @@
 
 from math import sqrt
 
+def is_prime(number):
+    """Tests if a number is prime"""
+    #ignore 1 as it isn't prime and skip even numbers
+    for i in range(3, int(sqrt(number)) + 2):
+        if number % i == 0:
+            return False
+    return True
 
 def factorList(number):
+    """Returns all odd factors above 1 for a given number"""
     factors = []
     #ignore 1 as it isn't prime and skip even numbers
     for i in range(3, int(sqrt(number)) + 2):
-            if number % i == 0:
-                factors.append(i)
-                factors.append(number/i)
+        # If we have the first of number of the two factors we can work out 
+        # the other i.e. if the number is 10 and we have 2 we can work out 5 is
+        # the other number for 2 x 5 to make 10
+        # this saves looping past the sqrt of the number
+        if number % i == 0:
+            factors.append(i)
+            factors.append(number/i)
     return factors
 
+# First get the odd factors for 600851475143
 factors = factorList(600851475143)
-largest_prime = 0
-for factor in factors:
-    if len(factorList(factor)) == 0:
-        if factor > largest_prime:
-            largest_prime = factor
 
-print largest_prime
+primes = []
+# Loop through the odd factors of the large number and check if they are prime
+for factor in factors:
+    if is_prime(factor):
+        primes.append(factor)
+
+print(f"The largest prime is {max(primes)}")
